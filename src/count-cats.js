7d3346301e0fdb4d73d5096d2d6f68109b08bda4
Andrii Shopiak <andriyshopyak@gmail.com>
2023-04-15 21:35:07 +0300

    feat: add count of cats algoritm

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0;
+  if (matrix.length == 0) return 0;
+for (let i = 0; i < matrix.length; i++) {
+    matrix[i].reduce((a,b) => {
+        if (a == '^^' || b == '^^') {
+            count++;
+        }
+    },[]);
+}
+return count;
 }
 
 module.exports = {
