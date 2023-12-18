ad3a4a122d21d9b5b339ed5e12e3f91021879b09
Andrii <andriyshopyak@gmail.com>
2023-12-18 19:41:06 +0200

    feat: add solution for vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.value = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let arrayOfKeys = key.toUpperCase().split("").map((el) => {
+      return el.charCodeAt(0) - "A".charCodeAt(0);
+    });
+    let messageArr = message.toUpperCase().split("");
+    let indexOfKey = 0;
+    let res = [];
+    let index = 0;
+
+    while (index <= messageArr.length -1) {
+      if (messageArr[index].toUpperCase() !== messageArr[index].toLowerCase()) {
+        res.push(String.fromCharCode("A".charCodeAt(0) + (messageArr[index].charCodeAt(0) - "A".charCodeAt(0) + arrayOfKeys[indexOfKey % arrayOfKeys.length]) % 26));
+        indexOfKey += 1;
+      } else {
+        res.push(messageArr[index]);
+      }
+      index += 1;
+    }
+
+    if(this.value) {
+      return res.join("");
+    }
+    return res.reverse().join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let arrayOfKeys = key.toUpperCase().split("").map((el) => {
+      return el.charCodeAt(0) - "A".charCodeAt(0);
+    });
+    let messageArr = message.toUpperCase().split("");
+    let indexOfKey = 0;
+    let res = [];
+    let index = 0;
+
+    while (index <= messageArr.length -1) {
+      if (messageArr[index].toUpperCase() !== messageArr[index].toLowerCase()) {
+        res.push(String.fromCharCode("A".charCodeAt(0) + (messageArr[index].charCodeAt(0) - "A".charCodeAt(0) - arrayOfKeys[indexOfKey % arrayOfKeys.length] + 26) % 26));
+        indexOfKey += 1;
+      } else {
+        res.push(messageArr[index]);
+      }
+      index += 1;
+    }
+
+    if(this.value) {
+      return res.join("");
+    }
+    return res.reverse().join("");
   }
 }
 
